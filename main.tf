181810d05ebb85e5a26688281bc525487d016a6f
Ware-Em-Out <DL_Ware@outlook.com>
2023-06-20 15:39:07 -0400

    Added newline to main.tf

--- main.tf
+++ main.tf
@@ -17,4 +17,5 @@ resource "aws_instance" "app_server" {     # We will be launching a EC2 instance
   ami           = "ami-022e1a32d3f742bd8"  # Name of Amazon Machine Image (i.e. VM) 
   instance_type = "t2.micro"               # Type of instance
   }
+
   
